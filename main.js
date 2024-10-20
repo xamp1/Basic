c6a07b8fdc53075ae9f1601bab8c5f22aee2f8a1
xamp1 <aththaariq0@gmail.com>
2024-10-21 00:05:52 +0700

    add-falling-animation

--- main.js
+++ main.js
@@ -25,29 +25,51 @@ const scene = new THREE.Scene();
 
         camera.position.z = 5;
 
+        let donuts = [];
         let mouseX = 0;
         let mouseY = 0;
         const windowHalfX = window.innerWidth / 2;
         const windowHalfY = window.innerHeight / 2;
 
         document.addEventListener('mousemove', onDocumentMouseMove);
+        container.addEventListener('click', onDonutClick);
 
         function onDocumentMouseMove(event) {
             mouseX = (event.clientX - windowHalfX) / 100;
             mouseY = (event.clientY - windowHalfY) / 100;
         }
 
+        function onDonutClick(event) {
+            // Clone the current state of the main donut
+            const newDonut = new THREE.Mesh(geometry.clone(), material.clone());
+            newDonut.position.set(donut.position.x, donut.position.y, donut.position.z);
+            newDonut.rotation.set(donut.rotation.x, donut.rotation.y, donut.rotation.z);
+            donuts.push(newDonut);
+            scene.add(newDonut);
+        }
+
         function animate() {
             requestAnimationFrame(animate);
 
+            // Update main donut position based on mouse
             donut.position.x += (mouseX - donut.position.x) * 0.05;
             donut.position.y += (-mouseY - donut.position.y) * 0.05;
 
             donut.rotation.x += 0.01;
             donut.rotation.y += 0.01;
 
+            // Animate falling donuts
+            donuts.forEach((d, index) => {
+                d.position.y -= 0.02; // Gravity effect
+                if (d.position.y < -5) { 
+                    scene.remove(d); 
+                    donuts.splice(index, 1); // Remove the donut if it's out of view
+                }
+            });
+
             renderer.render(scene, camera);
         }
+
         animate();
 
         window.addEventListener('resize', () => {
